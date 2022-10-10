2ec0394f9259970e6aea14066e39c1c9eea917d5
hgtphang <phanghungtai@gmail.com>
2022-10-10 17:46:19 -0400

    Part 2 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotNull
+    @Size(min=2, max=70)
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
